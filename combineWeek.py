4163ea57d76d371899ea733640cde63d81c3c62d
ltzhao <tumi_ever@163.com>
2016-06-28 14:06:31 +0800

    6th version
    主要更改了mac2poi.py：用引号注释的部分是另一个作用

--- combineWeek.py
+++ combineWeek.py
@@ -21,26 +21,26 @@ def getDict(s):
 
 if __name__=="__main__":
     #s1="~/Documents/data/bupt/apnum/commercial/apnum0316_commercial"
-    s1="0316"
+    s1="0323"
     dict1 = getDict(s1)
-    s2="0317"
+    s2="0324"
     dict2 = getDict(s2)
-    s3="0318"
+    s3="0325"
     dict3 = getDict(s3)
-    s4="0319"
+    s4="0326"
     dict4 = getDict(s4)
-    s5="0320"
+    s5="0327"
     dict5 = getDict(s5)
-    s6="0321"
+    s6="0328"
     dict6 = getDict(s6)
-    s7="0322"
+    s7="0329"
     dict7 = getDict(s7)
 
     a = set(dict3.keys()).intersection(set(dict1.keys()).intersection(set(dict2.keys())))
     b = set(dict7.keys()).intersection(set(dict6.keys()).intersection(set(dict5.keys())))
     c = set(dict4.keys()).intersection(a.intersection(b))
 
-    fw = open('apnum16_22','w')
+    fw = open('apnum23_29','w')
     for k in c:
         fw.write(k+' ')
         l = dict1[k]
--- mac2poi.py
+++ mac2poi.py
@@ -14,23 +14,49 @@ def getMacDict(s):
     fr.close()
     return macDict
 
+def getMacList(s):
+    fr = open(s,'r')#apnum16_22
+
+    macList = []
+    for l in fr.readlines():
+        l = l.strip('\n')
+        l = l.split(' ')
+        macList.append(l[0])
+    fr.close()
+    return macList
+
 
 if __name__=="__main__":
     #s1="~/Documents/data/bupt/apnum/commercial/apnum0316_commercial"
-    s1="apnum16_22_star2endhour"
+    '''
+    s1="apnum23_29"
+    list1 = getMacList(s1)
+    fw = open('buptpoi23_29','w')
+    fr2=open('../bupt_poi','r')
+    for line in fr2.readlines():
+        l = line.strip('\n')
+        l = l.split('|')
+        #ssid = l[0]
+        mac = l[1]
+        if mac in list1:
+            fw.write(line)
+    fw.close()
+
+    '''
+    s1="apnum23_29"
     dict1 = getMacDict(s1)
 
     no = {} #no[ssid]=clusterNo.
-    fr3 = open('ssid16_22','r')
+    fr3 = open('ssid23_29','r')
     for l in fr3.readlines():
         l = l.strip('\n')
         l = l.split(',')
         no[l[0]]=l[1]
     fr3.close()
 
-    fr2 = open('buptpoi16_22','r')
+    fr2 = open('buptpoi23_29','r')
     #fw = open('apnumpoi16_22','w')
-    fw = open('apnumpoi16_22_star2endhour','w')
+    fw = open('apnumpoi23_29','w')
     for l in fr2.readlines():
         l = l.strip('\n')
         l = l.split('|')
